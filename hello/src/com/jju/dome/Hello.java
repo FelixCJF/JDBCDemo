4e1fe1e946c05bc781bebd0ba7f4ab81f0a9137c
jianfengchen999 <jianfengchen999@gmail.com>
2016-10-20 17:39:59 +0800

    提交

--- hello/src/com/jju/dome/Hello.java
+++ hello/src/com/jju/dome/Hello.java
@@ -1,9 +1,11 @@
 package com.jju.dome;
 
+
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("你好！ 世界");
+		System.out.println("haha");
 	}
 	
 }
